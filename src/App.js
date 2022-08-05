615341223ec9ab8c8e1ce0cd005a29d46fbc8256
umer-farooq <urfq_farooq@hotmail.com>
2022-08-05 21:07:04 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,40 +4,50 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import { Routes, Route } from "react-router-dom";
 import About from './components/About';
-import Alert from './components/Alert';
+import Alert from './components/Alert'
+import ReactUseEffect from './components/ReactUseEffect';
+import ReactUseContext from './components/ReactUseContext';
 
 const App = () => {
   const [alert, setAlert] = useState(null)
   const [mode, setMode] = useState("light")
   const showAlert = (massage, type) => {
     setAlert({
-      massage:massage,
-      type:type
+      massage: massage,
+      type: type
     })
   }
-  
+
   const toggleMode = () => {
-    if(mode === "light"){
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#042743"
-      showAlert("Dark mode has been enable" , "success")
-    }else {
+      showAlert("Dark mode has been enable", "success")
+    } else {
       setMode("light")
       document.body.style.backgroundColor = "white"
-      showAlert("Light mode has been enable" , "success")
+      showAlert("Light mode has been enable", "success")
     }
   }
-  setTimeout(()=>{
-      setAlert(null)
+  setTimeout(() => {
+    setAlert(null)
   }, 1800)
   return (
     <>
-      <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <Routes>
-        <Route path="/" element={<TextForm heading = "Enter text to analyze" mode={mode} showAlert={showAlert}/>} />
-        <Route path="/about" element={<About mode={mode}/>} />
+        <Route path="/"
+          mode={mode}
+          element={
+            <TextForm heading="Enter text to analyze"
+              showAlert={showAlert} />
+          }
+        />
+        <Route path="/about" element={<About mode={mode} />} />
       </Routes>
+      <ReactUseEffect />
+      <ReactUseContext />
     </>
   );
 }
